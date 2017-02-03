311c0ab83aec18a58a20c796e149b3aadf9d7703
Gabriel Mainginski <gabriel.1519@hotmail.com>
2017-02-03 11:47:48 -0200

    Comparação por LBP

--- Sem Titulo2.cpp
+++ Sem Titulo2.cpp
@@ -82,6 +82,7 @@ IplImage * Histograma(IplImage* src, int index) {
         cvGetMinMaxHistValue( histo[index][2], &min_value, &max_value);
         for( b = 0; b < 255; b++ )
         {
+            //Distância euclidiana;
             int value = cvQueryHistValue_1D( histo[index][2], b);
             int normalized = cvRound(value*50/max_value);
             cvLine(hist_img,cvPoint(b,150), cvPoint(b,150-normalized), CV_RGB(0,0,255));
@@ -89,6 +90,62 @@ IplImage * Histograma(IplImage* src, int index) {
 
     return hist_img;
 }
+
+IplImage* lbp(IplImage* src){
+    CvScalar center, code;
+     IplImage* dist = cvCreateImage(cvGetSize(src), src->depth, 1);
+     IplImage* image_tar = cvCreateImage(cvGetSize(dist), dist->depth, 1);
+     cvCvtColor(src, dist, CV_RGB2GRAY);
+
+    for(int i=1; i < src->height-1; i++){
+		    for(int j=1; j < src->width-1; j++) {
+               center = cvGet2D(dist, i, j);
+		       code.val[0] = 0;
+		       if(center.val[0] <= cvGet2D(dist, i-1, j-1).val[0]) code.val[0] += 128;
+		       if(center.val[0] <= cvGet2D(dist, i-1, j).val[0])   code.val[0] += 64;
+		       if(center.val[0] <= cvGet2D(dist, i-1, j+1).val[0]) code.val[0] += 32;
+		       if(center.val[0] <= cvGet2D(dist, i, j+1).val[0])   code.val[0] += 16;
+		       if(center.val[0] <= cvGet2D(dist, i+1, j+1).val[0]) code.val[0] += 8;
+		       if(center.val[0] <= cvGet2D(dist, i+1, j).val[0])   code.val[0] += 4;
+		       if(center.val[0] <= cvGet2D(dist, i+1, j-1).val[0]) code.val[0] += 2;
+		       if(center.val[0] <= cvGet2D(dist, i, j-1).val[0])   code.val[0] += 1;
+			  cvSet2D(image_tar, i, j, code);
+
+		     }
+		}
+		return image_tar;
+}
+
+IplImage * Histograma_lbp(IplImage* src, int index) {
+    IplImage* g_plane = lbp(src);
+        int hist_size[] = {256}, channels = {0};
+        /* hue varies from 0 (~0 deg red) to 180 (~360 deg red again) */
+        float min_value, max_value;
+    // Set ranges for histogram bins
+    float lranges[] = {0, 255};
+    float* ranges[] = {lranges};
+        //int scale = 10;
+        IplImage* hist_img = cvCreateImage( cvSize(255,50),8,1 );
+        //CvHistogram* hist;
+        int r;
+
+        //cvCvtColor( src, hsv, CV_BGR2HSV );
+        //cvCvtColor( src, hsv, CV_HSV2BGR );
+        cvZero( hist_img );
+        histo[index][0] = cvCreateHist( 1, hist_size, CV_HIST_ARRAY, ranges );
+        cvCalcHist( &g_plane, histo[index][0]);
+        cvGetMinMaxHistValue( histo[index][0], &min_value, &max_value);
+        printf("\n min max %f %f \n", min_value, max_value);
+        for( r = 0; r < 255; r++ )
+        {
+            int value = cvQueryHistValue_1D( histo[index][0], r);
+            int normalized = cvRound(value*50/max_value);
+            cvLine(hist_img,cvPoint(r,50), cvPoint(r,50-normalized), CV_RGB(128,128,128));
+        }
+
+    return hist_img;
+}
+
 int somaArray(int hist[][3][256], int canal, int quadrante){
     int aux=0;
     for(int i=0;i<255;i++){
@@ -127,31 +184,7 @@ void equalizaHistograma(int hist[][3][256],int escalas,int equalizado[][3][256])
         }
 }
 
-IplImage* lbp(IplImage* src){
-    CvScalar center, code;
-    printf("%d %d \n", src->height, src->width);
-     IplImage* dist = cvCreateImage(cvGetSize(src), src->depth, 1);
-     IplImage* image_tar = cvCreateImage(cvGetSize(dist), dist->depth, 1);
-     cvCvtColor(src, dist, CV_RGB2GRAY);
 
-    for(int i=1; i < src->height-1; i++){
-		    for(int j=1; j < src->width-1; j++) {
-               center = cvGet2D(dist, i, j);
-		       code.val[0] = 0;
-		       if(center.val[0] <= cvGet2D(dist, i-1, j-1).val[0]) code.val[0] += 128;
-		       if(center.val[0] <= cvGet2D(dist, i-1, j).val[0])   code.val[0] += 64;
-		       if(center.val[0] <= cvGet2D(dist, i-1, j+1).val[0]) code.val[0] += 32;
-		       if(center.val[0] <= cvGet2D(dist, i, j+1).val[0])   code.val[0] += 16;
-		       if(center.val[0] <= cvGet2D(dist, i+1, j+1).val[0]) code.val[0] += 8;
-		       if(center.val[0] <= cvGet2D(dist, i+1, j).val[0])   code.val[0] += 4;
-		       if(center.val[0] <= cvGet2D(dist, i+1, j-1).val[0]) code.val[0] += 2;
-		       if(center.val[0] <= cvGet2D(dist, i, j-1).val[0])   code.val[0] += 1;
-			  cvSet2D(image_tar, i, j, code);
-
-		     }
-		}
-		return image_tar;
-}
 
 float perc[4][3][10];
 int hist[4][3][256],equalizado[4][3][256];
@@ -161,9 +194,12 @@ IplImage *dst, *imgS;
 CvScalar v;
 
 main(){
-int escolha;
-printf("Escolha um numero de imagem para comparar com as outras: ");
+    IplImage * Imh2, * imgSel;
+int escolha, modo;
+printf("Escolha um numero de imagem para comparar com as outras: \n> ");
 scanf("%d", &escolha);
+printf("\nEm qual modo voce gostaria de comparar?\n1-Percentis de cor\n2-LBP\n3-Mix\n> ");
+scanf("%d", &modo);
 int img;
 //int hsize[] = {256};
 sprintf(buf, "%d", escolha);
@@ -174,11 +210,18 @@ strcat(cwd_imagens, "\\image.orig\\");
 strcat(cwd_imagens, buf);
 sprintf(path, cwd_imagens, escolha);
 imgS = cvLoadImage(path, CV_LOAD_IMAGE_COLOR);
-IplImage * imgSel = Histograma(imgS, escolha);
+switch(modo){
+        case 1:
+            imgSel = Histograma(imgS, escolha);
+        break;
+        case 2:
+            imgSel = Histograma_lbp(imgS, escolha);
+        break;
+}
 
 //histograma = cvCreateHist(3,hsize,CV_HIST_ARRAY);
 for (img = 0; img < 1000; img++) {
-        if(img == escolha){img++;}
+        if(img == escolha){continue;}
         //cvClearHist(histograma);
         sprintf(buf, "%d", img);
         strcat(buf, ".jpg");
@@ -193,29 +236,7 @@ for (img = 0; img < 1000; img++) {
         //cvZero(imagemHistogramaEqualizada);
         /*
         zeraHistograma(hist, equalizado);
-        for (int i = 0; i < dst->height; i++)
-             for (int j = 0; j < dst->width; j++) {
-                    v = cvGet2D(dst, i, j);
-                    if (i <= dst->height / 2 && j <= dst->width / 2) { //1 quadrante
 
-                        hist[0][0][(int) v.val[0]]++; //R
-                        hist[0][1][(int) v.val[1]]++; //G
-                        hist[0][2][(int) v.val[2]]++; //B
-
-                    } else if (i > dst->height / 2 && j <= dst->width / 2) { //2 quadrante
-                        hist[1][0][(int) v.val[0]]++; //R
-                        hist[1][1][(int) v.val[1]]++; //G
-                        hist[1][2][(int) v.val[2]]++; //B
-                    } else if (i <= dst->height / 2 && j > dst->width / 2) { //3 quadrante
-                        hist[2][0][(int) v.val[0]]++; //R
-                        hist[2][1][(int) v.val[1]]++; //G
-                        hist[2][2][(int) v.val[2]]++; //B
-                    } else { //4 quadrante
-                        hist[3][0][(int) v.val[0]]++; //R
-                        hist[3][1][(int) v.val[1]]++; //G
-                        hist[3][2][(int) v.val[2]]++; //B
-                   }
-         } // for
 
         equalizaHistograma(hist,120,equalizado);*/
 
@@ -245,15 +266,24 @@ for (img = 0; img < 1000; img++) {
         fprintf(arquivo, "\n");
         fclose(arquivo);*/
 
-     cvShowImage("Imagem", dst);
+     cvShowImage("Imagem",dst);
 
     // Converte imagem original colorida para níveis de cinza
 
-
-	IplImage * Imh2 = Histograma(dst, img);
-   // Mostra Histograma
-    cvShowImage("Histograma", Imh2);
-    comparations[img] = cvCompareHist(histo[img][0], histo[escolha][0],0) + cvCompareHist(histo[img][1], histo[escolha][1],0) + cvCompareHist(histo[img][2], histo[escolha][2],0);
+    switch(modo){
+        case 1:
+            Imh2 = Histograma(dst, img);
+           // Mostra Histograma
+            cvShowImage("Histograma", Imh2);
+            comparations[img] = cvCompareHist(histo[img][0], histo[escolha][0],0) + cvCompareHist(histo[img][1], histo[escolha][1],0) + cvCompareHist(histo[img][2], histo[escolha][2],0);
+        break;
+        case 2:
+            Imh2 = Histograma_lbp(dst, img);
+           // Mostra Histograma
+            cvShowImage("Histograma", Imh2);
+            comparations[img] = cvCompareHist(histo[img][0], histo[escolha][0],0);
+        break;
+    }
     printf("Imagem %d: %f\n",img, comparations[img]);
     //cvWaitKey(5000);
 
